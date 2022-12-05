a575b848bdfb226308234cb0c49f15faaed2373b
CsalaPeter <csalapeter95@gmail.com>
2022-12-05 12:48:10 +0100

    Day one part 2 finished

--- 2022/Day_1/day1.js
+++ 2022/Day_1/day1.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 
 const data = fs.readFileSync("input.txt", "utf8");
 let maxCalories = 0;
+let allCal = [];
+let top3 = [0, 0, 0];
 
 data?.split("\n\n").forEach((elf) => {
   const elfCalories = elf
@@ -10,9 +12,17 @@ data?.split("\n\n").forEach((elf) => {
       (accumulator, currentValue) => accumulator + parseInt(currentValue),
       0
     );
+  allCal.push(elfCalories);
   if (elfCalories > maxCalories) {
     maxCalories = elfCalories;
   }
 });
 
 console.log(maxCalories);
+top3 = allCal.sort((a, b) => b - a).slice(0, 3);
+top3Sum = top3.reduce(
+  (accumulator, currentValue) => accumulator + parseInt(currentValue),
+  0
+);
+console.log(top3);
+console.log(top3Sum);
